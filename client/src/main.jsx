f002ed30a8814da1b9d4d652981bf3190ebfd997
Abdurrahman-Kavak <apokvk03@gmail.com>
2026-05-16 16:55:41 +0300

    eklenen ToastContainer kullanıldı

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-toastify/dist/ReactToastify.css";
+import { Bounce, ToastContainer } from "react-toastify";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
